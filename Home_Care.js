010ec1349ddf84156c15ddadd37af660cc0e57e4
Matheus2henrique <matheus2006henriquelopes@gmail.com>
2025-11-12 13:47:58 -0500

    alterando header

--- Home_Care.js
+++ Home_Care.js
@@ -138,3 +138,13 @@
       requestAnimationFrame(animation);
     });
   });
+
+
+   window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) { 
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
